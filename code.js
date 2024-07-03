343a35e77c9bd1876a2e1d65bb4a6e45c961a5ea
Gustavo Mello <gustavomello9178@gmail.com>
2024-07-03 16:10:24 -0300

    feat(code-page): Ordenação de um vetor, preservando a ordem dos elementos de inserção do outro

--- code.js
+++ code.js
@@ -6,3 +6,7 @@ for (i=0;i<5;i++){
     vet_sequencia.push(num) 
 
 }
+vet_ordenado.sort() // ordena o vetor
+console.log(vet_ordenado) // vetor ordenado
+console.log() // linha em branco
+console.log(vet_sequencia) // vetor direto na sequencia em que foi adicionado
